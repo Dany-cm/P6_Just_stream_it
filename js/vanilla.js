4432286edb575734d103a7ee3ed0d91299462934
Danycm1 <danycmwow@gmail.com>
2021-10-05 11:59:09 +0200

    JS: Error handling

--- js/vanilla.js
+++ js/vanilla.js
@@ -5,12 +5,22 @@ const api_url = "http://localhost:8000/api/v1/titles/"
 */
 function getBestRatedMovie() {
     fetch(api_url + '?sort_by=-imdb_score')
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                throw new Error("Something went wrong");
+            }
+        })
+        .then((data => {
             const best_rated_movie = data.results[0]
             document.getElementById("best-image").src = best_rated_movie.image_url
             document.getElementById("best-movie-title").innerText = best_rated_movie.title
-        })
+        }))
+        .catch((error) => {
+            console.log("Fetch Error :-S", error);
+        });
 }
 
 getBestRatedMovie()
