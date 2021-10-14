83e27c7eaa91d6ebc0a4839772e11dbcd380ec12
Danycm1 <danycmwow@gmail.com>
2021-10-14 17:42:18 +0200

    More work in js and css

--- js/vanilla.js
+++ js/vanilla.js
@@ -14,13 +14,53 @@ function getBestRatedMovie() {
             }
         })
         .then((data => {
-            const best_rated_movie = data.results[0]
-            document.getElementById("top-rated-movie-image").src = best_rated_movie.image_url
-            document.getElementById("top-rated-movie-title").innerText = best_rated_movie.title
+            displayMovie(data.results[0], "top-rated-movie")
         }))
         .catch((error) => {
             console.log("Fetch Error :-S", error);
         });
 }
 
+function displayMovie(retrievedMovie, divContainer) {
+    document.getElementById(divContainer + "-image").src = retrievedMovie.image_url
+    document.getElementById(divContainer + "-title").innerText = retrievedMovie.title
+}
+
+function initCarouselBestRated() {
+    fetch(api_url + '?sort_by=-imdb_score')
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                throw new Error("Something went wrong");
+            }
+        })
+        .then((data => {
+            addCarouselItem(data.results[0], "best-rated-movies-carousel")
+        }))
+        .catch((error) => {
+            console.log("Fetch Error :-S", error);
+        });
+}
+
+function addCarouselItem(movie, carouselDivId) {
+    const div_item = document.createElement("div");
+    //div_item.classList.add('best-rated-movies-h1');
+    const title = document.createElement('h1')
+    title.classList.add('best-rated-movies-h1');
+    const image = document.createElement('img')
+    //const description = document.createElement('p')
+
+    title.textContent = `${movie.title}`
+    image.src = `${movie.image_url}`
+    //description.textContent = `${movie.description}`
+
+    div_item.appendChild(title)
+    div_item.appendChild(image)
+    //div_item.appendChild(description)
+    document.getElementById(carouselDivId).appendChild(div_item)
+
+}
 getBestRatedMovie()
+initCarouselBestRated()
