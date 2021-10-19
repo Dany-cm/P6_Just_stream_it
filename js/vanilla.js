3f484a67197639a2eadf39a01b8a6716b082b584
Dany CM <danycmwow@gmail.com>
2021-10-19 21:59:43 +0200

    Commit unfinished work

--- js/vanilla.js
+++ js/vanilla.js
@@ -2,7 +2,7 @@ const api_url = "http://localhost:8000/api/v1/titles/"
 
 /* Display all movies and fill the carousel */
 getBestRatedMovie()
-initCarouselBestRated()
+fetchAndUpdate()
 
 /* Get the best rated movie on imdb */
 function getBestRatedMovie() {
@@ -23,86 +23,21 @@ function getBestRatedMovie() {
         });
 }
 
-/* Display the movie */
-function displayMovie(retrievedMovie, divContainer) {
-    document.getElementById(divContainer + "-image").src = retrievedMovie.image_url
-    document.getElementById(divContainer + "-title").innerText = retrievedMovie.title
+/* Fetch movies with parameters : genre, page, carouseldivid */
+function fetchMovies(genre, page) {
+    return fetch(`${api_url}?genre=${genre}&sort_by=-imdb_score&page=${page}`)
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                throw new Error("Something went wrong");
+            }
+        })
 }
 
-/* Init the carousel and retrieve movie information */
-function initCarouselBestRated() {
-    fetch(api_url + '?sort_by=-imdb_score')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            else {
-                throw new Error("Something went wrong");
-            }
-        })
-        .then((data => {
-            for (let i = 0; i < 4; ++i) {
-                addCarouselItem(data.results[i], "best-rated-movies-carousel")
-            }
-            
-        }))
-        .catch((error) => {
-            console.log("Fetch Error :-S", error);
-        });
-
-    fetch(api_url + '?genre=action' + '&sort_by=-imdb_score')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            else {
-                throw new Error("Something went wrong");
-            }
-        })
-        .then((data => {
-            for (let i = 0; i < 4; ++i) {
-                addCarouselItem(data.results[i], "action-category-movies-carousel")
-            }
-        }))
-        .catch((error) => {
-            console.log("Fetch Error :-S", error);
-        });
-
-    fetch(api_url + '?genre=horror' + '&sort_by=-imdb_score')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            else {
-                throw new Error("Something went wrong");
-            }
-        })
-        .then((data => {
-            for (let i = 0; i < 4; ++i) {
-                addCarouselItem(data.results[i], "horror-category-movies-carousel")
-            }
-        }))
-        .catch((error) => {
-            console.log("Fetch Error :-S", error);
-        });
-
-    fetch(api_url + '?genre=drama' + '&sort_by=-imdb_score')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            else {
-                throw new Error("Something went wrong");
-            }
-        })
-        .then((data => {
-            for (let i = 0; i < 4; ++i) {
-                addCarouselItem(data.results[i], "drama-category-movies-carousel")
-            }
-        }))
-        .catch((error) => {
-            console.log("Fetch Error :-S", error);
-        });
+function fetchAndUpdate() {
+    fetchMovies("adventure", 1).then(results => addCarouselItem(results, "action-category-movies-carousel"))
 }
 
 /* Add movies to the carousel */
@@ -117,7 +52,8 @@ function addCarouselItem(movie, carouselDivId) {
     document.getElementById(carouselDivId).appendChild(div_item)
 }
 
-function scrollContainer(direction) {
-    console.log('event', event)
-    console.log('direction', direction)
+/* Display the movie */
+function displayMovie(retrievedMovie, divContainer) {
+    document.getElementById(divContainer + "-image").src = retrievedMovie.image_url
+    document.getElementById(divContainer + "-title").innerText = retrievedMovie.title
 }
