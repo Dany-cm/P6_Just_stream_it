b6145ce34d23efd6d42c6d451dd8e540446706ae
Dany CM <danycmwow@gmail.com>
2021-10-20 17:10:41 +0200

    More work

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,27 +1,7 @@
 const api_url = "http://localhost:8000/api/v1/titles/"
 
 /* Display all movies and fill the carousel */
-getBestRatedMovie()
 fetchAndUpdate()
-
-/* Get the best rated movie on imdb */
-function getBestRatedMovie() {
-    fetch(api_url + '?sort_by=-imdb_score')
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-            else {
-                throw new Error("Something went wrong");
-            }
-        })
-        .then((data => {
-            displayMovie(data.results[0], "top-rated-movie")
-        }))
-        .catch((error) => {
-            console.log("Fetch Error :-S", error);
-        });
-}
 
 /* Fetch movies with parameters : genre, page, carouseldivid */
 function fetchMovies(genre, page) {
@@ -37,7 +17,9 @@ function fetchMovies(genre, page) {
 }
 
 function fetchAndUpdate() {
-    fetchMovies("adventure", 1).then(results => addCarouselItem(results, "action-category-movies-carousel"))
+    //fetchMovies("adventure", 1).then(results => addCarouselItem(results, "aventure-category-movies-carousel"))
+    fetchMovies("", 1).then(results => addCarouselItem(results, "top-rated-movie"))
+
 }
 
 /* Add movies to the carousel */
