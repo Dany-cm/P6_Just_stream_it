b661ffccfd48e5e20210456d8ffed27485fd48e7
Dany CM <danycmwow@gmail.com>
2021-10-24 00:50:02 +0200

    Rewrite of the CSS nearly done, fetchings movies + carousel working

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,5 +1,6 @@
 const api_url = "http://localhost:8000/api/v1/titles/"
 
+
 /* Display all movies and fill the carousel */
 fetchAndUpdate()
 
@@ -18,7 +19,10 @@ function fetchMovies(genre, page) {
 
 /*  Call fetchMovies with selected parameters and add it to the carousel */
 function fetchAndUpdate() {
-    fetchMovies("", 1).then(movie => addCarouselItem(movie.results[0], "top-rated-movie"))
+    fetchMovies("", 1).then(movie => {
+        addCarouselItem(movie.results[0], "top-rated-movie")
+        document.getElementsByClassName("movie-title")[0].innerText = movie.results[0].title
+    })
     fetchMovies("", 1).then(movie => {
         for (movie of movie.results) {
             addCarouselItem(movie, "best-rated-movies")
@@ -43,18 +47,57 @@ function fetchAndUpdate() {
 
 /* Add movies to the carousel */
 function addCarouselItem(movie, carouselDivId) {
-    const div_item = document.createElement("div");
-    const image = document.createElement('img')
+    /* Create a div and img element */
+    const divItem = document.createElement("div");
+    const image = document.createElement('img');
 
+    /* Set image source to the movie url and alt to the title*/
     image.src = `${movie.image_url}`
+    image.alt = `Image de couverture du film ${movie.title}`
 
-    div_item.appendChild(image)
+    /* Add a class img to the div and add image to it */
+    divItem.setAttribute("class", "image")
+    divItem.appendChild(image)
 
-    document.getElementById(carouselDivId).appendChild(div_item)
+    /* Query the path to the movies-carousel and add all information */
+    const carousel = document.querySelector(`#${carouselDivId} > div > div.movies-carousel`)
+    carousel.append(image)
 }
 
+/* Update carousel's movies */
+function updateCarouselItem(movie, carouselDivId) {
+    /* Get img element */
+    const image = document.querySelector(`#${carouselDivId} > div > div > img`);
+
+    /* Set image source to the movie url and alt to the title*/
+    image.src = `${movie.image_url}`
+    image.alt = `Image de couverture du film ${movie.title}`
+
+    /* Query the path to the movies-carousel and add all information */
+    const carousel = document.querySelector(`#${carouselDivId} > div > div.movies-carousel`)
+    carousel.append(image)
+}
+
+
+const pageCounter = []
+pageCounter['best-rated-movies'] = { page: 1, genre: "" }
+pageCounter['adventure-category-movies'] = { page: 1, genre: 'adventure' }
+pageCounter['horror-category-movies'] = { page: 1, genre: 'horror' }
+pageCounter['crime-category-movies'] = { page: 1, genre: 'crime' }
+
+/* Function to handle arrows */
 function scrollContainer(container, direction) {
-    console.log('event', event)
-    console.log('container', container)
-    console.log('direction', direction)
+    if (direction == "next") {
+        fetchMovies(pageCounter[container].genre, ++pageCounter[container].page).then(movie => {
+            for (movie of movie.results) {
+                updateCarouselItem(movie, container)
+            }
+        })
+    } else if (direction == "previous" && pageCounter[container].page != 1) {
+        fetchMovies(pageCounter[container].genre, --pageCounter[container].page).then(movie => {
+            for (movie of movie.results) {
+                updateCarouselItem(movie, container)
+            }
+        })
+    }
 }
