16c158f983e94986dfee5f77f5d8865c61dbd2b6
Dany CM <danycmwow@gmail.com>
2021-10-25 00:54:43 +0200

    Add a modal and fix a slider bug

--- js/vanilla.js
+++ js/vanilla.js
@@ -54,10 +54,13 @@ function addCarouselItem(movie, carouselDivId) {
     /* Set image source to the movie url and alt to the title*/
     image.src = `${movie.image_url}`
     image.alt = `Image de couverture du film ${movie.title}`
+    image.id = `${movie.id}`
 
-    /* Add a class img to the div and add image to it */
-    divItem.setAttribute("class", "image")
-    divItem.appendChild(image)
+    image.setAttribute('id', `${movie.id}`)
+    image.onclick = onImgClicked
+
+    /* Add add image to the div */
+    divItem.append(image)
 
     /* Query the path to the movies-carousel and add all information */
     const carousel = document.querySelector(`#${carouselDivId} > div > div.movies-carousel`)
@@ -72,12 +75,12 @@ function updateCarouselItem(movie, carouselDivId) {
     /* Set image source to the movie url and alt to the title*/
     image.src = `${movie.image_url}`
     image.alt = `Image de couverture du film ${movie.title}`
+    image.id = `${movie.id}`
 
     /* Query the path to the movies-carousel and add all information */
     const carousel = document.querySelector(`#${carouselDivId} > div > div.movies-carousel`)
     carousel.append(image)
 }
-
 
 const pageCounter = []
 pageCounter['best-rated-movies'] = { page: 1, genre: "" }
@@ -99,5 +102,43 @@ function scrollContainer(container, direction) {
                 updateCarouselItem(movie, container)
             }
         })
+    }
+}
+
+/* Modal */
+const onImgClicked = function (event) {
+    const modal = document.getElementById("myModal");
+    const movie_id = event.target.getAttribute('id')
+    var span = document.getElementsByClassName("close-modal")[0];
+    fetch(`${api_url}${movie_id}`)
+        .then(res => res.json())
+        .then(movie => {
+            document.getElementsByClassName('movie-img')[0].src = movie.image_url
+            document.getElementsByClassName("movie-titles")[0].textContent = `Titre: ${movie.title}`
+            document.getElementsByClassName("movie-genre")[0].textContent = `Genre(s): ${movie.genres}`
+            document.getElementsByClassName("movie-release-date")[0].textContent = `Date de sortie: ${movie.date_published}`
+            document.getElementsByClassName("movie-rated")[0].textContent = `Rated: ${movie.rated}`
+            document.getElementsByClassName("movie-rating-imdb")[0].textContent = `Score IMDB: ${movie.imdb_score}`
+            document.getElementsByClassName("movie-director")[0].textContent = `Réalisateur(s): ${movie.directors}`
+            document.getElementsByClassName("movie-list-actors")[0].textContent = `Liste des acteurs: ${movie.actors}`
+            document.getElementsByClassName("movie-duration")[0].textContent = `Duration : ${movie.duration} minutes`
+            document.getElementsByClassName("movie-country-origin")[0].textContent = `Origine(s) : ${movie.countries}`
+            document.getElementsByClassName("movie-box-office-result")[0].textContent = `Résultat du box office: ${movie.worldwide_gross_income ?? 'Non disponible'}`
+            document.getElementsByClassName("movie-description")[0].textContent = `Synopsis: ${movie.description}`
+
+            /* Display the modal */
+            modal.style.display = "block"
+        })
+    
+    /* Close the modal when clicking on the button */
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    /* Close the modal when clicking anywhere */
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
